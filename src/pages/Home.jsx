84c628aa2261b4b4e0c39acb5b9c6cd0d640883a
Clinton Rizzo <70792821+clintrizzo@users.noreply.github.com>
2022-09-03 11:56:16 -0700

    created slider page with data

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -5,12 +5,13 @@ import Footer from "../components/Footer";
 import Navbar from "../components/Navbar";
 // import Newsletter from "../components/Newsletter";
 // import Products from "../components/Products";
-// import Slider from "../components/Slider";
+import Slider from "../components/Slider";
 
 const Home = () => {
   return (
     <div>
       <Navbar />
+      <Slider />
       <Footer />
     </div>
   );
